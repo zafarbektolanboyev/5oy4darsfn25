d08ca1e05506390b206559ef088cbca7471b08ba
zafarbek tolanboyev <zafarvektolanboyev618@gmail.com>
2024-06-24 13:47:34 +0500

    comet

--- index.js
+++ index.js
@@ -16,42 +16,42 @@
 // img.setAttribute('alt', 'Bu yerda tabiat rasmi bor');
 // console.log(img)
 
-//let p = document.querySelector('#hi');
+// let p = document.querySelector('#hi');
 // p.innerHTML = "xayr";
 // console.log(p.innerHTML);
-//console.log(p.textContent);//textContent taglarr ichidagi textni olib beradi
+// console.log(p.textContent);//textContent taglarr ichidagi textni olib beradi
 
 
 // web page
-// const btns = document.querySelector('.buttons');
-// const block = document.querySelector('.block')
-// const body = document.querySelector('#body')
-// const white = document.querySelector('#white');
-// const dark = document.querySelector('#dark');
-// const auto = document.querySelector('#auto');
+const btns = document.querySelector('.buttons');
+const block = document.querySelector('.block')
+const body = document.querySelector('#body')
+const white = document.querySelector('#white');
+const dark = document.querySelector('#dark');
+const auto = document.querySelector('#auto');
 
-// white.addEventListener('click', function(){
-//     white.style.body = 'white';
-//     white.style.color = 'black';
-// })
-// dark.addEventListener('click', function(){
-//     body.style.backgroundColor = 'black';
-//     body.style.color = 'white';
-//     dark.style.backgroundColor = 'white';
-//     dark.style.color = 'black';
-// })
-// white.addEventListener('click', function(){
-//     body.style.backgroundColor = 'white';
-//     body.style.color = 'black';
-//     dark.style.backgroundColor = 'black';
-//     dark.style.color = 'white';
-// })
-// auto.addEventListener('click', function(){
-//     body.style.backgroundColor = 'wheat';
-//     body.style.color = 'black';
-//     dark.style.backgroundColor = 'black';
-//     dark.style.color = 'white';
-// })
+white.addEventListener('click', function(){
+    white.style.body = 'white';
+    white.style.color = 'black';
+})
+dark.addEventListener('click', function(){
+    body.style.backgroundColor = 'black';
+    body.style.color = 'white';
+    dark.style.backgroundColor = 'white';
+    dark.style.color = 'black';
+})
+white.addEventListener('click', function(){
+    body.style.backgroundColor = 'white';
+    body.style.color = 'black';
+    dark.style.backgroundColor = 'black';
+    dark.style.color = 'white';
+})
+auto.addEventListener('click', function(){
+    body.style.backgroundColor = 'wheat';
+    body.style.color = 'black';
+    dark.style.backgroundColor = 'black';
+    dark.style.color = 'white';
+})
 
 
 
